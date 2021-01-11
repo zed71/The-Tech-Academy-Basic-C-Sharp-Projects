1224c3afa2c7735075fe815eaf290e52e6b4a53e
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-11 13:35:55 -0800

    Updated Code and Text
    Added What course are you on.  Added correct boolean and integers.

--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -7,7 +7,37 @@ namespace VariablesAndDataTypes
     {
         static void Main()
         {
-            
+            // this is a simple program 
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char randomLetter = 'X';
+            //decimal moneyInBank = 100.5m;
+            //double heightInCentinmeteres = 211.30202092;
+            //float secondsLeft = 2.62f;
+            //short temperatureOnMars = -341;
+
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
+
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+
+            //Console.WriteLine(yearsOld);
+            //Console.ReadLine();
+
             Console.WriteLine("The Tech Academy \nStudent Daily Report");
             Console.ReadLine(); 
 
@@ -16,28 +46,33 @@ namespace VariablesAndDataTypes
             Console.WriteLine("Your name is: " + yourName);
             Console.ReadLine();
 
+            Console.WriteLine("What course are you on?");
+            string courseName= Console.ReadLine();
+            Console.WriteLine("Your course is: " + courseName);
+            Console.ReadLine();
+
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your page number is: " + pageNumber);
             Console.ReadLine();
 
             Console.WriteLine("Do you need help with anything? Please anwser \"true\" or \"false.\"");
-            string needHelp = Console.ReadLine();
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Your reply is: " + needHelp);
             Console.ReadLine();
 
             Console.WriteLine("Were there any positive experiences? Please give specifics.");
             string posExp = Console.ReadLine();
-            Console.WriteLine(posExp + "\nThank you for sharing!!! ");
+            Console.WriteLine("\nThank you for sharing!!! ");
             Console.ReadLine();
 
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific");
             string specFeedback = Console.ReadLine();
-            Console.WriteLine(specFeedback + "\nWe appreciate your feedback " + yourName);
+            Console.WriteLine("\nWe appreciate your feedback " + yourName);
             Console.ReadLine();
 
             Console.WriteLine("How many hours did you study today?");
-            string studyHours= Console.ReadLine();
+            int studyHours = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Nice work today studying: " + studyHours);
             Console.ReadLine();
 
