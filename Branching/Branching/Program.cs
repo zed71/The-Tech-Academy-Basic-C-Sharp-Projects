945685507e62c869df70f6b7965101b0a3099875
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-12 21:46:06 -0800

    Commit updated files for Branching

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -69,37 +69,52 @@ namespace Branching
             //Console.WriteLine(result);
 
 
-            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
-            
-
-            Console.WriteLine("Please enter the package weight:");
-            int packWeight = Convert.ToInt32(Console.ReadLine());
-            int actWeight = 50;
-            if (packWeight > actWeight)
-            {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
-            }
-
-            Console.WriteLine("Please enter package width:");
-            int packWidth = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter package height:");
-            int packHeight = Convert.ToInt32(Console.ReadLine());
-            int actHeight = 50;
-            if (packHeight > actHeight)
-            {
-                Console.WriteLine("Package to big to be shipped via Package Express");
-            }
-
-            Console.WriteLine("Please enter package length:");
-            int packLength = Convert.ToInt32(Console.ReadLine());
-
-            int sum = packWeight * packWidth * packHeight * packLength;
-            Console.WriteLine(sum / 100);
-
-            Console.WriteLine("Your estimated total for shipping is: $528.00");
-
+            //introduction
+            Console.WriteLine("Welcome to Package Express. " +
+                "Please follow the instructions below: ");
             Console.ReadLine();
+
+            //takes weight input from user
+            Console.WriteLine("Please enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            //Checks if weight is too high
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. " +
+                    "Have a good day.");
+                Console.ReadLine();
+            }
+            //if the package is less than 50, continue with the process
+            else
+            {
+                Console.WriteLine("Please enter the package width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height: ");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                //checks the package meets the volume requirement
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                //if the package height is less than 50, continue with the process
+                else
+                {
+                    //variable included in this 'else' statement because
+                    //they are local to each block of the branches. 
+                    //Next time, store globally....
+                    int shippingCost = ((width * height * length) * weight) / 100;
+                    Console.WriteLine("Your estimated shipping cost for this package " +
+                        "is: $" + shippingCost + ".00 \nThank you!");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
