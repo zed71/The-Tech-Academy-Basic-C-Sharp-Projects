5c2c7445a8eefd1c78989ccac42cecccdceca9d6
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-17 23:23:28 -0800

    Commit Void Method Page 228
    C# & .NET FRAMEWORK COURSE
    Page 228 Void Method

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiate the class
+            VoidMethod obj1 = new VoidMethod();
+
+            // Call the method in the class, passing in 2 numbers
+            obj1.Assign(10, 3);
+            Console.ReadLine();
+
+            // Call the method in the class, specify parameters by name
+            // This changes the default values to specified ones
+            obj1.Assign(y: 10, x: 20);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/VoidMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class VoidMethod
+    {
+        // Create a void method that takes 2 assigned parameters
+        public void Assign(int x, int y)
+        {
+            // Do a math operation on parm 1
+            int sum = x + 10;
+            Console.WriteLine(x + " + 10 = " + sum);
+
+            // Display int parm 2 to screen
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
