ef9677e5a87fe4d344eab60c2cf0386757da52ea
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-18 12:30:41 -0800

    Commit StaticMethod Challenge page 231
    C# & .NET FRAMEWORK COURSE
    Page 231

---
+++ StaticMethod/StaticMethod/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    static class Operations
+    {
+        // Create a void method that outputs an int
+        // Data passed into it is divided by 2
+        public static void DividedBy(int x)
+        {
+            int product = x / 2;
+            Console.WriteLine(x + " / 2 = " + product);
+        }
+
+        // Create a method with output parameters
+        // Overload a method
+        // Declare class static
+        public static void DividedBy(out int x)
+        {
+            x = 30;
+            x += x;
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Non-static instantiation
+            //Operations obj1 = new Operations();
+
+            Console.WriteLine("Input a number to be divided by 2:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            // Static instantiation
+            Operations.DividedBy(x);
+
+            // Call overloaded method with output 
+            Operations.DividedBy(out x);
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+    }
+}
