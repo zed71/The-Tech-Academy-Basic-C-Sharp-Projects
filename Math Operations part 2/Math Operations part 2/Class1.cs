5e25c95545580268824c01603abb19e6205829a9
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-17 22:38:25 -0800

    Commit code for page 226 Challenge
    C# & .NET FRAMEWORK COURSE
    Page 226

---
+++ Math Operations part 2/Math Operations part 2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operations_part_2
+{
+    class Operations
+    {
+        // Create a math operation class for an int
+        public int Assignment(int x)
+        {
+            int sum = x + 5;
+            return sum;
+        }
+
+        public decimal Assignment(decimal x)
+        {
+            int difference = (int)(x - 5);
+            return difference;
+        }
+
+        public int Assignment(string x)
+        {
+            int length = Int32.Parse(x);
+            int product = length * 10;
+            return product;
+        }
+    }
+}
---
+++ Math Operations part 2/Math Operations part 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operations_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demonstrates Method Overloading
+            // Instantiate the class and pass in an int
+            Operations obj1 = new Operations();
+            obj1.Assignment(5);
+            Console.WriteLine(obj1.Assignment(5));
+            Console.ReadLine();
+
+            // Passing in decimal, returning int
+            Operations obj2 = new Operations();
+            obj2.Assignment(12.75m);
+            Console.WriteLine(obj2.Assignment(12.75m));
+            Console.ReadLine();
+
+            // Passing in string, return ing int if possible
+            Operations obj3 = new Operations();
+            obj3.Assignment("10");
+            Console.WriteLine(obj3.Assignment("10"));
+            Console.ReadLine();
+
+        }
+        
+    }
+}
