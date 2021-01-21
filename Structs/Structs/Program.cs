6325ffc01fc2f2d6538c562865dead2da64fc531
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-21 02:38:52 -0800

    Commit Structs assignment page 267
    C# & .NET FRAMEWORK COURSE
    Page 267 Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Number num = new Number();
+            num.Amount = 5150m;
+
+            Console.WriteLine("Amount: " + num.Amount);
+            Console.Read();
+
+
+
+        }
+    }
+}
