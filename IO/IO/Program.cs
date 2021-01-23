0f8f07d058723b415bf7911971d96722912ad5ee
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-23 00:17:42 -0800

    Commit IO assignment page 297
    C# & .NET FRAMEWORK COURSE
    Page 297 IO logs

---
+++ IO/IO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.WriteLine("Hi there! Please enter a number, and I will " +
+                "create a text file of it for you:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Log the number to a text file
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Brandon\Documents\logs\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.ReadLine();
+
+            // Print the file back to the user
+            string text = File.ReadAllText(@"C:\Users\Brandon\Documents\logs");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
