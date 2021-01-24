edb1e9ac50542d0b80c99ef3481c22979120e233
BrandonF <37319101+zed71@users.noreply.github.com>
2021-01-23 23:50:21 -0800

    Commit Assignment page 310
    C# & .NET FRAMEWORK COURSE
    Page 310 assignment Constant Var and Constructor String

---
+++ ConstVar/ConstVar/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Person
+    {
+        public Person(string userFName) : this(userFName, "Fuhrer")
+        {
+        }
+        public Person(string userFName, string userLName)
+        {
+            FName = userFName;
+            LName = userLName;
+        }
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
+}
+
+
